d4d60adc032e3919b8afdfcfa6e936030ae2e2fd
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-12-02 09:40:32 +0000

    update with working code

--- a/tests/test_open_file.rs
+++ a/tests/test_open_file.rs
@@ -1,13 +1,7 @@
-use std::{fs, path};
+use std::fs;
 
 #[test]
 fn test_open_file() {
-    let file = path::PathBuf::from(file!());
-    let dir = path::absolute(file.parent().unwrap()).unwrap();
-    let file_path = format!("{}/file.txt", dir.display());
-    println!("{}", file.display());
-    println!("{}", dir.display());
-    println!("{}", file_path);
-    let contents = fs::read_to_string(file_path).unwrap();
+    let contents = fs::read_to_string("tests/file.txt").unwrap();
     println!("{contents}");
 }
